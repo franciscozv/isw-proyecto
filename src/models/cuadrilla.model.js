7c83d91118f409283bdaf948631d6bb8c370b0fa
ReneSanchezH <rene.sanchez2001@alumnos.ubiobio.cl>
2023-05-23 06:18:54 -0400

    correcciónes menores

--- src/models/cuadrilla.model.js
+++ src/models/cuadrilla.model.js
@@ -1,10 +1,59 @@
 const mongoose = require("mongoose");
 
+/*Nombre: Cuadrilla + número
+Ej: Cuadrilla 1, Cuadrilla 2, etc. */
 const cuadrillaSchema = new mongoose.Schema({
-    nombre: {
-        type: String,
-        require: true,
-    },
+  nombre: {
+    type: String,
+    require: true,
+    minLength: 1,
+    maxLength: 14,
+  },
+  jefeCuadrilla: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Brigadista",
+    required: false,
+  },
+  operadorMotobomba: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Brigadista",
+    required: false,
+  },
+  operadorMotosierra: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Brigadista",
+    required: false,
+  },
+  combatiente: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Brigadista",
+    required: false,
+  },
+  encargadoPrimerosAuxilios: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Brigadista",
+    required: false,
+  },
+  asistentePrimerosAuxilios: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Brigadista",
+    required: false,
+  },
+  encargadoHerramientas: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Brigadista",
+    required: false,
+  },
+  ayudanteHerramientas: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Brigadista",
+    required: false,
+  },
+  radioperador: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Brigadista",
+    required: false,
+  },
 });
 
 const Cuadrilla = mongoose.model("Cuadrilla", cuadrillaSchema);
